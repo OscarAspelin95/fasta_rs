e5c39d429ef9a0b395d17c1731c2439f79bea40a
Oscar Aspelin <oscar.aspelin@1928diagnostics.com>
2025-07-31 14:57:03 +0200

    Add homopolymer counter

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -1,12 +1,19 @@
 use crate::args::{App, SubCommand};
 
 use crate::head::fasta_head;
+use crate::homopolymers::fasta_homopolymers;
 use crate::stats::fasta_stats;
 
 pub fn dispatch(args: App) {
     match args.command {
-        SubCommand::Stats { fasta, outfile } => fasta_stats(&fasta, &outfile),
-        SubCommand::Head { fasta, num_seqs } => fasta_head(&fasta, num_seqs),
+        SubCommand::Stats { fasta, outfile } => fasta_stats(&fasta, &outfile).unwrap(),
+        SubCommand::Head { fasta, num_seqs } => fasta_head(&fasta, num_seqs).unwrap(),
+        SubCommand::Homopolymers {
+            fasta,
+            min_hp_len,
+            strict,
+            outfile,
+        } => fasta_homopolymers(&fasta, min_hp_len, strict, &outfile).unwrap(),
         _ => todo!(),
     };
 }
